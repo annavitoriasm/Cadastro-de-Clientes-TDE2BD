e6f1b4910bcaff95677963106a4988eb89b6eaf3
Anna Vitoria <161900970+annavitoriasm@users.noreply.github.com>
2024-11-09 15:26:40 -0300

    sidebar

---
+++ src/script/sidebar.js
@@ -0,0 +1,9 @@
+const nav = document.querySelectorAll('.navbar');
+const btn = document.getElementById('open-nav');
+let idk = 0;
+
+function close() {
+    nav[idk].classList.toggle('close')
+};
+
+btn.addEventListener('click', close);
