e9a5af4e7b8b6fb0e4010206d5be5896a945312c
Anna Vitoria <161900970+annavitoriasm@users.noreply.github.com>
2024-11-09 16:43:00 -0300

    telinha de carregamento

---
+++ src/script/loading.js
@@ -0,0 +1,9 @@
+function loading() {
+    document.getElementById("box-load").style.display = "none";
+
+    const content = document.getElementById("content");
+    content.classList.add("show");
+}
+window.onload = function() {
+    setTimeout(loading, 2500);
+};
