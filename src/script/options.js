c256bf01db05904441a0246bb2c3e894e95bf5a5
Anna Vitoria <161900970+annavitoriasm@users.noreply.github.com>
2024-11-23 15:44:45 -0300

    adição de opções na estrutura clientes

---
+++ src/script/options.js
@@ -0,0 +1,24 @@
+const options = document.getElementById("options");
+        const openOptionsButton = document.getElementById("open-options");
+
+        function opOptions(event){
+            // Impede o evento de propagação para o document
+            event.stopPropagation();
+
+            if (options.classList.contains('hidden')) {
+                options.classList.remove('hidden');
+                options.classList.add('flex');
+            }
+        }
+
+        function clOptions(){
+            options.classList.remove('flex');
+            options.classList.add('hidden');
+        }
+
+        // Fecha o menu se o clique for fora do menu de opções e do botão de abrir
+        document.addEventListener('click', function(event) {
+            if (!options.contains(event.target) && !openOptionsButton.contains(event.target)) {
+                clOptions();
+            }
+        });
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}",
     '*.{html,js}'],
   theme: {
-    extend: {},
+    keyframes: {
+      
+    }
   },
   plugins: [
   ],
