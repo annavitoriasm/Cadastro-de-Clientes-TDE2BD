9c43c127076ed76aba244476cb588482f868df5f
Anna Vitoria <161900970+annavitoriasm@users.noreply.github.com>
2024-11-25 20:54:38 -0300

    ajuste container criação de cliente

--- src/script/register.js
+++ src/script/register.js
@@ -1,11 +1,17 @@
 const modal = document.getElementById("create");
 
 function openC(){
+    modal.classList.remove('clDiv');
     modal.classList.remove('hidden');
     modal.classList.add('flex');
+    modal.classList.add('opDiv')
 }
 
 function closeC(){
-    modal.classList.remove('flex');
-    modal.classList.add('hidden');
+    modal.classList.remove('opDiv');
+    modal.classList.add('clDiv');
+    setTimeout(function() {
+        modal.classList.add('hidden');
+        modal.classList.remove('flex');
+      }, 1000);
 }
